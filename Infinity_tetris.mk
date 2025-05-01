3ba19c7346b06a82570815ba006a8fe1e708f793
Nitesh <91332849+hemlo69@users.noreply.github.com>
2025-05-01 06:57:59 +0530

    Update Infinity_tetris.mk

--- Infinity_tetris.mk
+++ Infinity_tetris.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common DroidX-UI stuff.
-$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from tetris device
 $(call inherit-product, device/nothing/tetris/device.mk)
